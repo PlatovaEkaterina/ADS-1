1f70ffc7873aac9782f822611915f79adaa0084d
PlatovaEkaterina <75724059+PlatovaEkaterina@users.noreply.github.com>
2021-03-15 17:52:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,11 @@ int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
 }
-
         int cnt = 0;
         for (int i = 0; i < size; i++) {
                 if (arr[i] == value)
                         cnt += 1;
 }
-
         if (cnt != 0)
                 return cnt;
         else
