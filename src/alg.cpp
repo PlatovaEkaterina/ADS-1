0b8176af00019349b0433e8b38c1e25b63798db1
PlatovaEkaterina <75724059+PlatovaEkaterina@users.noreply.github.com>
2021-03-17 19:34:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
-}
+
+  int r = size - 1, l = 0, count = 0;
+    while (l < r) {
+        int m = (r + l) / 2;
+        if (arr[m] < value)
+            l = m + 1;
+        else
+            r = m;
+    }
+
+    if (arr[l] == value) {
+        while (arr[l] == value) {
+            count++;
+            l++;
+        }
+    }
